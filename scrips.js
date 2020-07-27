557f7ebf398b6ddbc18fe2d9f00b5689b0b0dbae
Icaro Rodrigues <icaro.cerqueira@cjr.org.br>
2020-07-27 19:04:42 -0300

    Adiciona alterações no layout

--- scrips.js
+++ scrips.js
@@ -1,31 +1,36 @@
-fetch('https://api.instabuy.com.br/apiv3/layout?subdomain=organicos', {
-    method: 'GET',
 
-}).then(res => res.json())
-    .then(data => {
+try {
 
-        const products = data.data.collection_items
-        const banners = data.data.banners
+    fetch('https://api.instabuy.com.br/apiv3/layout?subdomain=organicos', {
+        method: 'GET',
 
-        document.getElementById('banners').innerHTML =
+    }).then(res => res.json())
+        .then(data => {
 
-            `${banners.map(banner => (
+            console.log(data)
 
-                `<div key=${banner.id}>
+
+            const products = data.data.collection_items
+            const banners = data.data.banners
+
+            document.getElementById('banners').innerHTML =
+
+                `${banners.map(banner => (
+
+                    `<div key=${banner.id}>
                         <div class="banner_image">
                             <img src=${`https://assets.instabuy.com.br/ib.store.banner/bnr-${banner.image}`} alt=${banner.title}/>    
                         </div>
                 </div>`
-            )).join('')
-            }`
+                )).join('')
+                }`
 
-        document.getElementById('products').innerHTML =
+            document.getElementById('products').innerHTML =
 
-            `${products.map(product => (
+                `${products.map(product => (
+                    product.items.map(item => (
 
-                product.items.map(item => (
-
-                    `<div key=${item.id} class="card_product">
+                        `<div key=${item.id} class="card_product">
                         <div class="card_image">
                         <img src=${`https://assets.instabuy.com.br/ib.item.image.large/l-${item.images[0]}`} alt=${item.name}/>    
                     </div>
@@ -33,11 +38,17 @@ fetch('https://api.instabuy.com.br/apiv3/layout?subdomain=organicos', {
                     <div class="card_body">
                         ${item.brand === undefined ? '' : `<p><strong>Marca: </strong>${item.brand}</p>`}
                         <p><strong>Nome: </strong>${item.name}</p>
-                        <p><strong>Preço: </strong> R$ ${item.prices[0].price}</p>
+                        <p><strong>Preço: </strong> R$ ${item.prices[0].price} ${item.unit_type === "UNI" ? 'Uni.' : 'Kg.'}</p>
                     </div>
                     
                     </div>`
+                    )).join('')
                 )).join('')
-            )).join('')
-            }`
-    })
+                }`
+        })
+
+} catch (error) {
+
+    console.log(`error`)
+}
+
